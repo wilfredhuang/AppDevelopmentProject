a614e8d8f5577edbe52966154d29d735a9829fd2
NEW-U7V5P65AR1L\Tristan <tristan.chf@gmail.co>
2020-01-03 00:35:23 +0800

    clean up

--- app.py
+++ app.py
@@ -7,7 +7,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('userLogin.html')
+
+@app.route('/users/<email>')
+def users(email):
+    return 'Test successful, email is {}'.format(email)
+
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['email']
+        return redirect(url_for('users', email=user))
+
 
 
 if __name__ == '__main__':
