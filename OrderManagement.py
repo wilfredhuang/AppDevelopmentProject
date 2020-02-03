2c120fcedf605fcfad5e3f62ef9468706c8d4223
TLI-Tristan <42214211+TLI-Tristan@users.noreply.github.com>
2020-02-03 18:05:27 +0800

    Update OrderManagement.py

--- OrderManagement.py
+++ OrderManagement.py
@@ -2,4 +2,13 @@ from ManagementSystem import ManagementSystem
 
 
 class OrderManagement(ManagementSystem):
-    pass
+
+    def __init__(self, storage_handler):
+        super().__init__("Orders", "Order Management", storage_handler)
+
+    def add_order(self):
+        pass
+
+    def update_order(self):
+        pass
+
