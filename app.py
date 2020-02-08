dde5ea8beb7d70bb8904ccdcd3d62a172f6e45f8
schwahue <hieuminhnguyen2027@gmail.com>
2020-02-08 18:11:38 +0800

    update homepage

--- app.py
+++ app.py
@@ -52,7 +52,10 @@ def get_sale():
 # Current is Login Page
 @app.route('/')
 def home():
-    ItemList = get_inventory().values()
+    itemDict = get_inventory().values()
+    ItemList = []
+    for i in itemDict:
+        ItemList.append(i)
     print(ItemList)
     return render_template('home.html', ItemList=ItemList)
 
